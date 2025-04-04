1c641cad925c44733ecccb81910a4604b47b11f0
bri <brenn.or.something@gmail.com>
2025-04-04 19:16:39 -0400

    idfk mane

--- src/main.lua
+++ src/main.lua
@@ -5,24 +5,46 @@
 ]]
 
 --REQUIRES--
+local SDL = require("SDL")
 
 --REQUIRES--
+local input = require("input.lua")
+
+--BLANK VARIABLE DEFS--
+local win = nil
+--BLANK VARIABLE DEFS---
 
 ScreenWidth = 640
 ScreenHeight = 480
+Title = "Gaycast"
+
+local running = false
 
 local function init()
+    assert(SDL.init(SDL.flags.Video, SDL.flags.Audio))
+    win = assert(SDL.createWindow{
+        title = Title,
+        width = ScreenWidth,
+        height = ScreenHeight,
+        x = 126,
+        y = 126
+    })
 
+    running = true
 end
 
 local function main()
-    while true do
-        
+    while running do
+        for e in SDL.pollEvent() do
+            if e.type == SDL.event.Quit then
+                running = false
+            end
+        end
     end
 end
 
 local function exit()
-
+    SDL.quit()
 end
 
 init()
---
+++ src/modules/input/input.lua
@@ -0,0 +1,7 @@
+local input = {}
+
+function input.checkPressedKey(key)
+    print("idk mane im not done with this")
+end
+
+return input
