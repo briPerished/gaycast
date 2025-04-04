b360f663cb2e73e3f2ca41adeacd94932a4dd257
bri <brenn.or.something@gmail.com>
2025-04-04 14:24:10 -0400

    first commit

---
+++ src/main.lua
@@ -0,0 +1,30 @@
+--[[
+
+:3
+
+]]
+
+--REQUIRES--
+
+--REQUIRES--
+
+ScreenWidth = 640
+ScreenHeight = 480
+
+local function init()
+
+end
+
+local function main()
+    while true do
+        
+    end
+end
+
+local function exit()
+
+end
+
+init()
+main()
+exit()
